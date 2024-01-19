f1c1565d2a4e5da8d9b585eb65f395624aaa9898
AlJClement <allisonclement@hotmail.ca>
2024-01-19 10:12:44 +0000

    adding run for irina to check prior to transfer

---
+++ run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=01:00:00
+#SBATCH --clusters=htc
+#SBATCH --gres=gpu:1
+
+module load Anaconda3
+source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env
+
+#run python code
+
+python ./utils/run_test.py --cfg ddh_meta
