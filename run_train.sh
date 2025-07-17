68f826b36565cd22145f6dbea71a8c8167af5c08
AlJClement <allisonclement@hotmail.ca>
2025-07-17 10:27:54 +0100

    working

--- run_train.sh
+++ run_train.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=22:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --clusters=htc
-#SBATCH --partition=medium
+#SBATCH --partition=short
 #SBATCH --mem-per-cpu=50G
+
 
 
 
@@ -13,4 +14,4 @@ module load Anaconda3
 source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
 
 #run python code
-python ./utils/run_training.py --cfg ddh_denoise
+python ./utils/run_training.py --cfg ddh_denoise_arc
