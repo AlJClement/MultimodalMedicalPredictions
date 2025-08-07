e99fc1332a03bb94327163e22ee0e8e085bb378b
AlJClement <allisonclement@hotmail.ca>
2025-08-07 15:40:11 +0100

    pull from clpc

--- run_test_hrnet.sh
+++ run_test_hrnet.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=10:00:00
 #SBATCH --clusters=htc
 #SBATCH --gres=gpu:v100:1
-#SBATCH --partition=short
+#SBATCH --partition=short,medium
 #SBATCH --mem-per-cpu=16G
 
 module load Anaconda3
@@ -13,4 +13,4 @@ source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
 
 #run python code
 
-python ./utils/run_test.py --cfg ddh_hrnet_arc
+python ./utils/run_test.py --cfg ddh_hrnet_arc_vbloss
--- run_train_hrnet.sh
+++ run_train_hrnet.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=20:00:00
 #SBATCH --clusters=htc
-#SBATCH --partition=long
+#SBATCH --partition=long,medium
 #SBATCH --mem-per-cpu=50G
 #SBATCH --gres=gpu:rtx8000:1
 
@@ -11,4 +11,4 @@ module load Anaconda3
 source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
 
 #run python code
-python ./utils/run_training.py --cfg ddh_hrnet_vbloss_arc
+python ./utils/run_training.py --cfg ddh_hrnet_arc
