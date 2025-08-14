f881a562d0a4da99b20a63ae25a48d7047a67113
AlJClement <allisonclement@hotmail.ca>
2025-08-14 16:59:19 +0100

    working with freeze hrnet

--- run_train_hrnet.sh
+++ run_train_hrnet.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=20:00:00
+#SBATCH --time=5:00:00
 #SBATCH --clusters=htc
-#SBATCH --partition=long,medium
+#SBATCH --partition=short,long,medium
 #SBATCH --mem-per-cpu=50G
 #SBATCH --gres=gpu:rtx8000:1
 
@@ -11,4 +11,4 @@ module load Anaconda3
 source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
 
 #run python code
-python ./utils/run_training.py --cfg ddh_hrnet_arc_alphafhc
+python ./utils/run_training.py --cfg ddh_hrnet_arc
