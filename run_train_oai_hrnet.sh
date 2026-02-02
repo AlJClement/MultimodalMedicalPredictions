7827feaa53e11a38d26edea7b94fefea961124c0
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-02-02 16:34:12 +0000

    Update submission files for arc

---
+++ run_train_oai_hrnet.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=15:00:00
+#SBATCH --clusters=htc
+#SBATCH --partition=long,medium
+#SBATCH --mem-per-cpu=50G
+#SBATCH --gres=gpu:rtx8000:1
+
+module load Anaconda3
+source activate $DATA/yenv
+
+#run python code
+python ./utils/run_training.py --cfg oai_arc_hrnet
