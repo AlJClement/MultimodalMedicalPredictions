fb900b4b7e8384196972f5e42a36d96bcd4b980e
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-02-02 22:21:22 +0000

    conda dont specify and run in interative works

--- run_train_oai_hrnet.sh
+++ run_train_oai_hrnet.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem-per-cpu=50G
 #SBATCH --gres=gpu:rtx8000:1
 
-module load Anaconda3/2025.06-1
+module load Anaconda3
 
 source activate /data/coml-oxmedis/kebl7678/yenv
 
--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem-per-cpu=50G
 #SBATCH --gres=gpu:rtx8000:1
 
-module load Anaconda3/2025.06-1
-source activate $DATA/yenv
+module load Anaconda3
+source activate /data/coml-oxmedis/kebl7678/yenv
 
 #run python code
 python ./utils/run_training.py --cfg oai_arc_unet
