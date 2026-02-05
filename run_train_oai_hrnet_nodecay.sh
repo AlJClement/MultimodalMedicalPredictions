ac6f325dc81b74cca48a106fbbec6d8ec075095c
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-02-05 17:35:19 +0000

    add decay and no decay files for ac

---
+++ run_train_oai_hrnet_nodecay.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=15:00:00
+#SBATCH --clusters=htc
+#SBATCH --partition=long,medium
+#SBATCH --mem-per-cpu=50G
+#SBATCH --gres=gpu:rtx8000:1
+
+module load Anaconda3/2022.05
+source activate /data/coml-oxmedis/kebl7678/yenv
+
+#run python code
+python ./utils/run_training.py --cfg oai_arc_hrnet_nodecay
