d2400ae2ce58832f3aca4fc3ef35fa188ce4c4ff
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-02-23 11:27:10 +0000

    add length of time for unet

--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --clusters=htc
 #SBATCH --partition=long,medium
 #SBATCH --mem-per-cpu=50G
