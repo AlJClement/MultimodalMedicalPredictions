e6f1879ad49fd0e8636d256dfd82aa5ec41c538d
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-03-09 12:43:39 +0000

    constraint fixed sku h not H

--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -5,7 +5,7 @@
 #SBATCH --clusters=htc
 #SBATCH --partition=long,medium
 #SBATCH --mem-per-cpu=50G
-#SBATCH --gres=gpu:1 --constraint='gpu_sku:H100'
+#SBATCH --gres=gpu:1 --constraint='gpu_sku:h100'
 
 
 module load Anaconda3/2022.05
