636a6d43cdf2184131cbe2bf044ca7229b90e159
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-03-09 12:49:25 +0000

    mem

--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -5,8 +5,8 @@
 #SBATCH --clusters=htc
 #SBATCH --partition=long,medium
 #SBATCH --mem-per-cpu=50G
-#SBATCH --gres=gpu:1 --constraint='gpu_mem:96GB'
-
+#SBATCH --gres=gpu:1 
+#SBATCH --constraint=gpu_mem_96GB
 
 module load Anaconda3/2022.05
 
