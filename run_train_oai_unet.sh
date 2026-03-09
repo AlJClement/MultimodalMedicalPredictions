a184577e6257e18eb5c701a5c3b32afc310f6172
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-03-09 09:59:18 +0000

    h100:4

--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -5,8 +5,8 @@
 #SBATCH --clusters=htc
 #SBATCH --partition=long,medium
 #SBATCH --mem-per-cpu=50G
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=h100
+#SBATCH --gres=gpu:h100:4
+
 module load Anaconda3/2022.05
 
 source activate /data/coml-oxmedis/kebl7678/yenv
