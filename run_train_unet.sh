43dd11be9c613ef462596993466fd99114f7bf83
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-03-09 21:13:30 +0000

    short nodes

--- run_train_unet.sh
+++ run_train_unet.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=15:00:00
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --clusters=htc
-#SBATCH --partition=long,medium
-#SBATCH --mem-per-cpu=50G
-#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --partition=short
+#SBATCH --mem-per-cpu=100G
 
 module load Anaconda3
 source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
