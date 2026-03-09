0648497dd8ade4efaa432aca9fe20b9e2b799d54
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-03-09 13:16:27 +0000

    nodelist

--- run_train_oai_unet.sh
+++ run_train_oai_unet.sh
@@ -4,8 +4,9 @@
 #SBATCH --time=48:00:00
 #SBATCH --clusters=htc
 #SBATCH --partition=long,medium
+#SBATCH --mem-per-cpu=50G
 #SBATCH --gres=gpu:1 
-#SBATCH --mem-per-gpu=80G
+#SBATCH --nodelist=htc-h[053-060]
 
 module load Anaconda3/2022.05
 
