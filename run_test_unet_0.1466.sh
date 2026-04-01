67fd4697604afd6c5911a375df94a62ab27f4bef
Allison Clement <allent@clpc125.cs.ox.ac.uk>
2026-04-01 16:12:24 +0100

    move to arc for test gpu

--- run_test_unet_0.1466.sh
+++ run_test_unet_0.1466.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=10:00:00
+#SBATCH --time=1:00:00
 #SBATCH --clusters=htc
 #SBATCH --partition=short
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=50G
+#SBATCH --gres=gpu:rtx8000:1
 
 module load Anaconda3/2022.05
 
--- run_test_unet_0.1466gradaccum.sh
+++ run_test_unet_0.1466gradaccum.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=10:00:00
+#SBATCH --time=1:00:00
 #SBATCH --clusters=htc
 #SBATCH --partition=short
-#SBATCH --mem-per-cpu=16G
-#SBATCH --gres=gpu:v100:1
-
+#SBATCH --mem-per-cpu=50G
 
 module load Anaconda3/2022.05
 
