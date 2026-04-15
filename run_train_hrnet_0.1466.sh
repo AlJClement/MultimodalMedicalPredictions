93c7d3a51236eaaf5a21e997a6a6fbc36fbe81f6
Allison Clement <allent@localhost>
2026-04-15 12:07:15 +0100

    add 0.1466 new run

--- run_train_hrnet_0.1466.sh
+++ run_train_hrnet_0.1466.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=48:00:00
+#SBATCH --time=11:50:00
 #SBATCH --gres=gpu:1
 #SBATCH --clusters=htc
 #SBATCH --mem-per-cpu=50G
-#SBATCH --gres=gpu:rtx8000:1
-
+#SBATCH --partition=short
 
 module load Anaconda3
 
-source activate /data/coml-oxmedis/kebl7678/conda_envs/mm_env2/mm_env
+source activate /data/coml-oxmedis/kebl7678/yenv
 #run python code
 python ./utils/run_training.py --cfg ddh_arc_newsplits_0.01466_hrnet
