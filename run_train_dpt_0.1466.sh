dba46f53f077997b92cae5a5a58f7c3fa7352ea6
Allison Clement <allent@localhost>
2026-04-20 10:46:36 +0100

    still fixing issue from before

--- run_train_dpt_0.1466.sh
+++ run_train_dpt_0.1466.sh
@@ -6,13 +6,13 @@
 #SBATCH --mem-per-cpu=50G
 #SBATCH --partition=short
 #SBATCH --gres=gpu:v100:1
+module load Anaconda3/2025.06-1
 
-module load Anaconda3
-
-source "$(conda info --base)/etc/profile.d/conda.sh"
+source /apps/system/easybuild/software/Anaconda3/2025.06-1/etc/profile.d/conda.sh
 conda activate /data/coml-oxmedis/kebl7678/yenv
 
-which python
+echo "Python: $(which python)"
 python -c "import torch; print(torch.__version__)"
+
 
 python ./utils/run_training.py --cfg ddh_0.01466_dpt
