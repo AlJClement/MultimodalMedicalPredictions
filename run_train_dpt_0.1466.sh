f37cafb987df2a6ff2821019fbd35b1f224b0658
Allison Clement <allent@localhost>
2026-04-20 10:41:30 +0100

    check why conda isnt loading venc torch

--- run_train_dpt_0.1466.sh
+++ run_train_dpt_0.1466.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
 #SBATCH --time=11:50:00
 #SBATCH --clusters=htc
 #SBATCH --mem-per-cpu=50G
@@ -9,7 +9,10 @@
 
 module load Anaconda3
 
-source activate /data/coml-oxmedis/kebl7678/yenv
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate /data/coml-oxmedis/kebl7678/yenv
 
-#run python code
+which python
+python -c "import torch; print(torch.__version__)"
+
 python ./utils/run_training.py --cfg ddh_0.01466_dpt
