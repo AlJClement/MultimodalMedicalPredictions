18955f5a61b291a96169b6fe931f5aa3203e7b58
Allison Clement <allent@localhost>
2026-04-24 15:59:21 +0100

    multimodal

---
+++ run_train_dpt_0.1466_mm_channelgate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=11:50:00
+#SBATCH --clusters=htc
+#SBATCH --mem-per-cpu=50G
+#SBATCH --partition=short
+#SBATCH --gres=gpu:v100:1
+
+module load Anaconda3/2022.05 
+source activate /data/coml-oxmedis/kebl7678/yenv
+
+#run python code
+python ./utils/run_training.py --cfg ddh_0.01466_dpt_swin
