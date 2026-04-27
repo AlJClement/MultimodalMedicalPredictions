29062f147df3acfc2a9665bcaed91e86ce82989c
Allison Clement <allent@localhost>
2026-04-27 14:52:02 +0100

    correct channel

--- run_train_dpt_0.1466_mm_channelgate.sh
+++ run_train_dpt_0.1466_mm_channelgate.sh
@@ -5,10 +5,10 @@
 #SBATCH --clusters=htc
 #SBATCH --mem-per-cpu=50G
 #SBATCH --partition=short
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:v100:1
 
 module load Anaconda3/2022.05 
 source activate /data/coml-oxmedis/kebl7678/yenv
 
 #run python code
-python ./utils/run_training.py --cfg ddh_0.01466_dpt_swin
+python ./utils/run_training.py --cfg ddh_0.01466_dpt_mm_channels
