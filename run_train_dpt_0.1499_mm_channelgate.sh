b38e1c0ae72f09efffc7c1b4b2e36a09960d8591
Allison Clement <allent@localhost>
2026-04-28 13:55:58 +0100

    move

--- run_train_dpt_0.1499_mm_channelgate.sh
+++ run_train_dpt_0.1499_mm_channelgate.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=20:00:00
 #SBATCH --clusters=htc
-#SBATCH --mem=50G
-#SBATCH --partition=gpu-v100
-#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --gres=gpu:v100:1
 
 module load Anaconda3/2022.05 
 source activate /data/coml-oxmedis/kebl7678/yenv
