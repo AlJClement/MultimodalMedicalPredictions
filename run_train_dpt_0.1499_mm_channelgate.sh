ae28b7f4769ac24ceecd27aa2f774911bdd8c554
Allison Clement <allent@localhost>
2026-04-28 13:50:44 +0100

    move with larger run time

--- run_train_dpt_0.1499_mm_channelgate.sh
+++ run_train_dpt_0.1499_mm_channelgate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=12:00:00
+#SBATCH --time=20:00:00
 #SBATCH --clusters=htc
 #SBATCH --mem-per-cpu=50G
-#SBATCH --partition=short
+#SBATCH --partition=medium
 #SBATCH --gres=gpu:v100:1
 
 module load Anaconda3/2022.05 
